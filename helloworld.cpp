e1be116a4f02080d893b106cd0b5ede8d8cdf644
Mrityunjay Kumar <mrityunjay7366@gmai.com>
2025-03-27 22:01:39 +0530

    Initial Commit

---
+++ helloworld.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 236;
+    cout << a << endl;
+    char ch = 's';
+    cout << ch << endl;
+    float f = 1.2;
+    cout << f << endl;
+    bool bl = true;
+    cout << bl << endl;
+    bool ble = false;
+    cout << ble << endl;
+    double d = 1.234;
+    cout << d << endl;
+    int size = sizeof(a);
+    cout << "size of a is : " << size << endl;
+    int c = 'c';
+    cout << c << endl;
+    return 0;
+}
